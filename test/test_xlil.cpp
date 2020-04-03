14ae34dbbe7395c57d391eae8bdc86df8ba4b2bb
Johan Mabille <johan.mabille@gmail.com>
2020-04-03 23:57:38 +0200

    Added CMake files and google tests

--- test/test_xlil.cpp
+++ test/test_xlil.cpp
@@ -1,16 +1,19 @@
+#include "gtest/gtest.h"
 #include <iostream>
 #include <xtensor-sparse/xlil.hpp>
 
-int main()
+namespace xt
 {
-    xt::xlil_container<double> A;
+    TEST(xlil, access_operator)
+    {
+        xt::xlil_container<double> A;
 
-    A(1, 0) = 3.;
-    A(1, 2) = 10.;
+        A(1, 0) = 3.;
+        A(1, 2) = 10.;
 
-    std::cout << A(1, 0) << "\n";
-    std::cout << A(1, 2) << "\n";
-    std::cout << A(3, 2) << "\n";
-
-    return 0;
+        std::cout << A(1, 0) << "\n";
+        std::cout << A(1, 2) << "\n";
+        std::cout << A(3, 2) << "\n";
+    }
 }
+
