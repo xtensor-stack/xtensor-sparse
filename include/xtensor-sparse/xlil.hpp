cb14bacc29f6c994639a0101785a136d7a56728d
gouarin <loic.gouarin@gmail.com>
2020-04-05 22:32:03 +0200

    implement resize and reshape

--- include/xtensor-sparse/xlil.hpp
+++ include/xtensor-sparse/xlil.hpp
@@ -9,6 +9,7 @@
 #include <xtensor/xstorage.hpp>
 #include <xtensor/xexception.hpp>
 #include <xtensor/xtensor.hpp>
+#include <xtensor/xadapt.hpp>
 #include <xtensor/xio.hpp>
 
 namespace xt
@@ -27,13 +28,29 @@ namespace xt
         using difference_type = std::ptrdiff_t;
 
         using shape_type = svector<size_type>;
+        using strides_type = svector<size_type>;
+        using backstrides_type = svector<size_type>;
 
         using inner_shape_type = svector<size_type>;
+        using inner_strides_type = svector<size_type>;
+        using inner_backstrides_type = svector<size_type>;
 
         xlil_container(const shape_type& shape);
 
+        size_type size() const noexcept;
+
         const shape_type& shape() const;
         size_type dimension() const;
+        
+        template <class S = shape_type>
+        void resize(S&& shape, bool force);
+        template <class S = shape_type>
+        void resize(S&& shape);
+
+        template <class S = shape_type>
+        auto& reshape(S&& shape) &;
+        template <class TT>
+        auto& reshape(std::initializer_list<TT> shape) &;
 
         template<class... Args>
         const_reference operator()(Args... args) const;
@@ -53,6 +70,13 @@ namespace xt
         template<class... Args>
         reference access_impl(Args... args);
 
+        template <class S = shape_type>
+        void reshape_impl(S&& shape, std::false_type);
+        template <class S = shape_type>
+        void reshape_impl(S&& shape, std::true_type);
+        template <class S = shape_type>
+        void update_keys(S&& shape);
+        
         shape_type m_shape;
         container_type m_data;
     };
@@ -65,6 +89,12 @@ namespace xt
      : m_shape{shape}
     {}
 
+    template <class T>
+    inline auto xlil_container<T>::size() const noexcept -> size_type
+    {
+        return compute_size(shape());
+    }
+
     template<class T>
     inline auto xlil_container<T>::shape() const -> const shape_type&
     {
@@ -75,6 +105,132 @@ namespace xt
     inline auto xlil_container<T>::dimension() const -> size_type
     {
         return m_shape.size();
+    }
+
+    template <class T>
+    template <class S>
+    inline void xlil_container<T>::resize(S&& shape, bool force)
+    {
+        std::size_t dim = shape.size();
+        if (m_shape.size() != dim || !std::equal(std::begin(shape), std::end(shape), std::begin(m_shape)) || force)
+        {
+            m_shape = xtl::forward_sequence<shape_type, S>(shape);
+        }
+    }
+
+    template <class T>
+    template <class S>
+    inline void xlil_container<T>::resize(S&& shape)
+    {
+        resize(std::forward<S>(shape), true);
+    }
+
+    template <class T>
+    template <class S>
+    inline auto& xlil_container<T>::reshape(S&& shape) &
+    {
+        reshape_impl(std::forward<S>(shape), std::is_signed<std::decay_t<typename std::decay_t<S>::value_type>>());
+        return *this;
+    }
+
+    template <class T>
+    template <class TT>
+    inline auto& xlil_container<T>::reshape(std::initializer_list<TT> shape) &
+    {
+        using sh_type = rebind_container_t<T, shape_type>;
+        sh_type sh = xtl::make_sequence<sh_type>(shape.size());
+        std::copy(shape.begin(), shape.end(), sh.begin());
+        reshape_impl(std::move(sh), std::is_signed<TT>());
+        return *this;
+    }
+    
+    template <class T>
+    template <class S>
+    inline void xlil_container<T>::reshape_impl(S&& shape, std::false_type /* is unsigned */)
+    {
+        if (compute_size(shape) != this->size())
+        {
+            XTENSOR_THROW(std::runtime_error, "Cannot reshape with incorrect number of elements. Do you mean to resize?");
+        }
+
+        update_keys(std::forward<S>(shape));
+        m_shape = xtl::forward_sequence<shape_type, S>(shape);
+    }
+
+    template <class T>
+    template <class S>
+    inline void xlil_container<T>::reshape_impl(S&& _shape, std::true_type /* is signed */)
+    {
+        using value_type = typename std::decay_t<S>::value_type;
+        auto new_size = compute_size(_shape);
+        if (this->size() % new_size)
+        {
+            XTENSOR_THROW(std::runtime_error, "Negative axis size cannot be inferred. Shape mismatch.");
+        }
+        std::decay_t<S> shape = _shape;
+        value_type accumulator = 1;
+        std::size_t neg_idx = 0;
+        std::size_t i = 0;
+        for(auto it = shape.begin(); it != shape.end(); ++it, i++)
+        {
+            auto&& dim = *it;
+            if(dim < 0)
+            {
+                XTENSOR_ASSERT(dim == -1 && !neg_idx);
+                neg_idx = i;
+            }
+            accumulator *= dim;
+        }
+        if(accumulator < 0)
+        {
+            shape[neg_idx] = static_cast<value_type>(this->size()) / std::abs(accumulator);
+        }
+        else if(this->size() != new_size)
+        {
+            XTENSOR_THROW(std::runtime_error, "Cannot reshape with incorrect number of elements. Do you mean to resize?");
+        }
+        
+        update_keys(shape);
+        m_shape = xtl::forward_sequence<shape_type, S>(shape);
+    }
+
+    template <class T>
+    template <class S>
+    inline void xlil_container<T>::update_keys(S&& shape)
+    {
+        container_type new_data;
+        for(auto& c: m_data)
+        {
+            auto& old_key = c.first;
+
+            // compute the offset for the old_key
+            size_type index = 0;
+            size_type accumulator = 1;
+            for (std::size_t i = m_shape.size(); i != 0; --i)
+            {
+                index += old_key[i - 1]*accumulator;
+                accumulator *= m_shape[i - 1];
+            }
+
+            // compute the new key from the offset and the new shape
+            index_type new_key = xtl::make_sequence<index_type>(shape.size());
+            accumulator = 1;
+            for(std::size_t i = 0; i < shape.size(); ++i)
+            {
+                accumulator *= static_cast<size_type>(shape[i]);
+            }
+            
+            for(std::size_t i = 0; i < shape.size(); ++i)
+            {
+                accumulator /= static_cast<size_type>(shape[i]);
+                new_key[i] = index/accumulator;
+                index -= new_key[i]*accumulator;
+            }
+
+            new_data[new_key] = c.second;
+        }
+
+        std::swap(m_data, new_data);
     }
 
     template<class T>
@@ -115,10 +271,9 @@ namespace xt
     inline auto xlil_container<T>::access_impl(Args... args) -> reference
     {
         // TODO: check if all args have the good type
-<<<<<<< HEAD
-        list_index_type key{args...};
-=======
         index_type key{static_cast<size_type>(args)...};
+
+        auto it = m_data.find(key);
         if (it == m_data.end())
             m_data[key] = ZERO;
         }
--- test/test_xlil.cpp
+++ test/test_xlil.cpp
@@ -15,6 +15,66 @@ namespace xt
         EXPECT_EQ(A.shape()[1], size_t(5));
     }
 
+    TEST(xlil, resize)
+    {
+        std::vector<std::size_t> shape{2, 5};
+        xt::xlil_container<double> A(shape);
+
+        std::vector<std::size_t> new_shape{20, 50};
+        A.resize(new_shape);
+        EXPECT_EQ(A.shape()[0], size_t(20));
+        EXPECT_EQ(A.shape()[1], size_t(50));
+    }
+
+    TEST(xlil, reshape_unsigned)
+    {
+        std::vector<std::size_t> shape{10};
+        xt::xlil_container<double> A(shape);
+
+        A(1) = 1.;
+        A(5) = 5.;
+        A(7) = 7.;
+
+        EXPECT_EQ(A.dimension(), size_t(1));
+        EXPECT_EQ(A.shape()[0], size_t(10));
+
+        std::vector<std::size_t> new_shape{2, 5};
+        A.reshape(new_shape);
+
+        EXPECT_EQ(A.dimension(), size_t(2));
+        EXPECT_EQ(A.shape()[0], size_t(2));
+        EXPECT_EQ(A.shape()[1], size_t(5));
+
+        EXPECT_EQ(A(0, 1), 1.);
+        EXPECT_EQ(A(1, 0), 5.);
+        EXPECT_EQ(A(1, 2), 7.);
+    }
+
+    TEST(xlil, reshape_signed)
+    {
+        std::vector<std::size_t> shape{20};
+        xt::xlil_container<double> A(shape);
+
+        A(1) = 1.;
+        A(5) = 5.;
+        A(7) = 7.;
+
+        EXPECT_EQ(A.dimension(), size_t(1));
+        EXPECT_EQ(A.shape()[0], size_t(20));
+
+        std::vector<int> new_shape{2, -1, 5};
+        A.reshape(new_shape);
+
+        EXPECT_EQ(A.dimension(), size_t(3));
+        EXPECT_EQ(A.shape()[0], size_t(2));
+        EXPECT_EQ(A.shape()[1], size_t(2));
+        EXPECT_EQ(A.shape()[2], size_t(5));
+
+        EXPECT_EQ(A(0, 0, 1), 1.);
+        EXPECT_EQ(A(0, 1, 0), 5.);
+        EXPECT_EQ(A(0, 1, 2), 7.);
+    }
+
     TEST(xlil, access_operator)
     {
         std::vector<std::size_t> shape{2, 5};
