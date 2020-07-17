ac43ad09f45786de59f2f1d604b5a0e75dbfd908
gouarin <loic.gouarin@gmail.com>
2020-07-17 11:38:34 +0200

    add xfunction implementation

---
+++ include/xtensor-sparse/xsparse_function.hpp
@@ -0,0 +1,92 @@
+#ifndef XSPARSE_FUNCTION_HPP
+#define XSPARSE_FUNCTION_HPP
+
+#include <xtl/xmeta_utils.hpp>
+#include <xtensor/xoperation.hpp>
+
+#include "xsparse_expression.hpp"
+
+namespace xt
+{
+    namespace extension
+    {
+        namespace detail
+        {
+            namespace mpl = xtl::mpl;
+            
+            template <class... CT>
+            using assign_tag_list = mpl::vector<get_assign_tag_t<CT>...>;
+
+            template <class... CT>
+            struct get_default_assign_tag
+            {
+                using tag_list = assign_tag_list<CT...>;
+                using type = std::conditional_t
+                             <
+                                    mpl::contains<tag_list, xdense_assign_tag>::value,
+                                    xdense_assign_tag,
+                                    xsparse_assign_tag
+                             >;
+            };
+
+            template <class CT>
+            struct get_default_assign_tag<CT>
+                : get_assign_tag<CT>
+            {
+            };
+
+            template <class... CT>
+            struct get_multiply_assign_tag
+            {
+                using tag_list = assign_tag_list<CT...>;
+                using type = std::conditional_t<
+                                    mpl::contains<tag_list, xsparse_assign_tag>::value,
+                                    xsparse_assign_tag,
+                                    xdense_assign_tag>;
+            };
+
+            template <class F, class... CT>
+            struct get_function_assign_tag : get_default_assign_tag<CT...>
+            {
+            };
+
+            template <class... CT>
+            struct get_function_assign_tag<xt::detail::multiplies, CT...>
+                : get_multiply_assign_tag<CT...>
+            {
+            };
+
+            template <class... CT>
+            struct get_function_assign_tag<xt::detail::divides, CT...>
+                : get_multiply_assign_tag<CT...>
+            {
+            };
+
+            template <class F, class... CT>
+            using get_function_assign_tag_t = typename get_function_assign_tag<F, CT...>::type;
+        }
+
+        template<class F, class... CT>
+        struct xfunction_sparse_base : public xsparse_empty_base<xfunction<F, CT...>>
+        {
+            using assign_tag = detail::get_function_assign_tag_t<F, CT...>;
+        };
+
+        template <class F, class... CT>
+        struct xfunction_base_impl<xsparse_expression_tag, F, CT...>
+        {
+            using type = xfunction_sparse_base<F, CT...>;
+        };
+    }
+
+    namespace detail
+    {
+        template <class F, class... E>
+        struct select_xfunction_expression<::xt::xsparse_expression_tag, F, E...>
+        {
+            using type = xfunction<F, E...>;
+        };
+    }
+}
+
+#endif
